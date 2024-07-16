402438d838d94aa84832ed8ec367d046ca2d49fc
moshesilver <moshetzvi03@gmail.com>
2024-07-15 21:10:18 -0400

    game works

--- Game.cs
+++ Game.cs
@@ -2,8 +2,10 @@
 using System.Drawing;
 using System.Text;
 
-namespace ConnectFour {
-    internal class Game {
+namespace ConnectFour
+{
+    internal class Game
+    {
         static Board board = Board.GetBoard();
         static Player redPlayer;
         static Player yellowPlayer;
@@ -14,7 +16,8 @@ namespace ConnectFour {
         static string redCircle = "🔴";
         static string yellowCircle = "🟡";
 
-        static void Main() {
+        static void Main()
+        {
             Console.InputEncoding = Encoding.UTF8;
             Console.OutputEncoding = Encoding.UTF8;
 
@@ -23,79 +26,99 @@ namespace ConnectFour {
             DisplayTurn();
             board.DisplayBoard();
             bool win;
-            do {
+            do
+            {
                 win = Turn();
             } while (!win);
         }
-        private static Player CurrentPlayer() {
+        private static Player CurrentPlayer()
+        {
             return turn == true ? redPlayer : yellowPlayer;
         }
-        public static void DisplayTurn() {
+        public static void DisplayTurn()
+        {
             Console.WriteLine($"\t    {CurrentPlayer().colorName} to move");
         }
-        public static bool Turn() {
-            // pick slot - CHECK IF COLUMN IS FULL AND VALID
+        public static bool Turn()
+        {
             int col = Methods.GetNumber2(1, 7, "Choose a column 1 - 7", exceptions: [.. filledCols]);
-            // update column
             colProgress[col - 1]++;
-            if (colProgress[col - 1] == 6) {
+            if (colProgress[col - 1] == 6)
+            {
                 filledCols.Add(col);
             }
-            // update slot
             board.slots[col - 1, 6 - (colProgress[col - 1])].color = CurrentPlayer().colorIcon;
-            // check for winner
             bool win = CheckForWinner(CurrentPlayer().colorIcon);
-            // if no winner - display turn and update bool turn
-            // if winner run winning code
-            // display board - either way
             Methods.ClearFullConsole();
+
+            Console.SetCursorPosition(0, Console.CursorTop - 1);
+            if (win)
+            {
+                Console.WriteLine($"{CurrentPlayer().name} WINS!!!");
+            }
+            else
+            {
+                turn = !turn;
+                DisplayTurn();
+            }
             board.DisplayBoard();
-            // switch player
-            turn = !turn;
 
             return win;
         }
-        public static bool CheckForWinner(string currentPlayerColor) {
+        public static bool CheckForWinner(string currentPlayerColor)
+        {
             // rows
-            for (int i = 0; i < board.slots.GetLength(0); i++) {
-                for (int j = 0; j < board.slots.GetLength(1) - 3; j++) {
+            for (int i = 0; i < board.slots.GetLength(0); i++)
+            {
+                for (int j = 0; j < board.slots.GetLength(1) - 3; j++)
+                {
                     if (board.slots[i, j].color == currentPlayerColor
                         && board.slots[i, j + 1].color == currentPlayerColor
                         && board.slots[i, j + 2].color == currentPlayerColor
-                        && board.slots[i, j + 3].color == currentPlayerColor) {
+                        && board.slots[i, j + 3].color == currentPlayerColor)
+                    {
                         return true;
                     }
                 }
             }
             // columns
-            for (int i = 0; i < board.slots.GetLength(0) - 3; i++) {
-                for (int j = 0; j < board.slots.GetLength(1); j++) {
+            for (int i = 0; i < board.slots.GetLength(0) - 3; i++)
+            {
+                for (int j = 0; j < board.slots.GetLength(1); j++)
+                {
                     if (board.slots[i, j].color == currentPlayerColor
                         && board.slots[i + 1, j].color == currentPlayerColor
                         && board.slots[i + 2, j].color == currentPlayerColor
-                        && board.slots[i + 3, j].color == currentPlayerColor) {
+                        && board.slots[i + 3, j].color == currentPlayerColor)
+                    {
                         return true;
                     }
                 }
             }
             // diagonal top-left to bottom-right
-            for (int i = 0; i < board.slots.GetLength(0) - 3; i++) {
-                for (int j = 0; j < board.slots.GetLength(1) - 3; j++) {
+            for (int i = 0; i < board.slots.GetLength(0) - 3; i++)
+            {
+                for (int j = 0; j < board.slots.GetLength(1) - 3; j++)
+                {
                     if (board.slots[i, j].color == currentPlayerColor
                         && board.slots[i + 1, j + 1].color == currentPlayerColor
                         && board.slots[i + 2, j + 2].color == currentPlayerColor
-                        && board.slots[i + 3, j + 3].color == currentPlayerColor) {
+                        && board.slots[i + 3, j + 3].color == currentPlayerColor)
+                    {
                         return true;
                     }
                 }
             }
             // diagonal top-right to bottom-left
-            for (int i = 3; i < board.slots.GetLength(0); i++) {
-                for (int j = 0; j < board.slots.GetLength(1) - 3; j++) {
+            for (int i = 3; i < board.slots.GetLength(0); i++)
+            {
+                for (int j = 0; j < board.slots.GetLength(1) - 3; j++)
+                {
                     if (board.slots[i, j].color == currentPlayerColor
                         && board.slots[i - 1, j + 1].color == currentPlayerColor
                         && board.slots[i - 2, j + 2].color == currentPlayerColor
-                        && board.slots[i - 3, j + 3].color == currentPlayerColor) {
+                        && board.slots[i - 3, j + 3].color == currentPlayerColor)
+                    {
                         return true;
                     }
                 }
